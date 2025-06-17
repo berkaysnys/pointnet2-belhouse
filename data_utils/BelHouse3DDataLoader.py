2d5145f3a27a107102bf86b3b9fe769d0757b816
berkaysnys <150937548+berkaysnys@users.noreply.github.com>
2025-06-17 18:37:43 +0300

    Update BelHouse3DDataLoader.py

--- data_utils/BelHouse3DDataLoader.py
+++ data_utils/BelHouse3DDataLoader.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset
 
 
 class BelHouse3DSemSegDataset(Dataset):
-    def __init__(self, root, split='train', num_points=2048, transform=None):
+    def __init__(self, root, split='train', num_points=2048, use_blocks=True, transform=None):
         """
         root: Path to IID-nonoccluded or OOD-occluded directory
         split: One of ['train', 'val', 'test']
@@ -15,10 +15,11 @@ class BelHouse3DSemSegDataset(Dataset):
         self.split = split
         self.num_points = num_points
         self.transform = transform
+        self.use_blocks = use_blocks
 
-        # Recursively find .npy files in blocks/ and rooms/
-        search_path = os.path.join(root, split, '**', '*.npy')
-        self.files = glob.glob(search_path, recursive=True)
+        subdir = 'blocks' if use_blocks else 'rooms'
+        search_path = os.path.join(root, split, subdir, '*.npy')
+        self.files = glob.glob(search_path)
 
         if len(self.files) == 0:
             raise RuntimeError(f"No .npy files found in {search_path}")
@@ -28,17 +29,17 @@ class BelHouse3DSemSegDataset(Dataset):
 
     def __getitem__(self, idx):
         file_path = self.files[idx]
-        data = np.load(file_path)  # Expected shape: (N, 4)
+        data = np.load(file_path) 
 
-        if data.shape[0] >= self.num_points:
-            indices = np.random.choice(data.shape[0], self.num_points, replace=False)
-        else:
-            indices = np.random.choice(data.shape[0], self.num_points, replace=True)
+        if self.use_blocks:
+            if data.shape[0] >= self.num_points:
+                indices = np.random.choice(data.shape[0], self.num_points, replace=False)
+            else:
+                indices = np.random.choice(data.shape[0], self.num_points, replace=True)
+            data = data[indices]
 
-        sampled = data[indices]
-
-        points = sampled[:, :3]  # x, y, z
-        labels = sampled[:, 3].astype(np.int64)
+        points = data[:, :3]
+        labels = data[:, 3].astype(np.int64)
 
         if self.transform:
             points = self.transform(points)
